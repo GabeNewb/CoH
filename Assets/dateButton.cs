ae6b3e818b50ac8b4ef08335bdf2eb73de5b3c74
Ph24588 <Ph24588@gmail.com>
2017-10-14 20:37:03 -0400

    Paul's first commit

---
+++ Assets/dateButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class dateButton : MonoBehaviour {
+	Button db;
+	// Use this for initialization
+	bool flag = false;
+
+	void Start () {
+		db = GetComponent<Button> ();
+	}
+
+	public void Press () {
+		if (flag == true) {
+			flag = false;
+			ColorBlock cb = db.colors;
+			cb.normalColor = new Color(0f, 1f, 1f, 1f);
+			db.colors = cb;
+		} else {
+			flag = true;
+			ColorBlock cb = db.colors;
+			cb.normalColor = new Color(0f, 0f, 1f, 1f);
+			db.colors = cb;
+		}
+	}
+}
